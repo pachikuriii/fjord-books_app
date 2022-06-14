fd511a0724b96c88bff835d5a7e2a4b6220faadc
pachikuriii <stakahashi0728@gmail.com>
2022-06-14 16:44:06 +0900

    不要なファイルを管理から削除

--- db/migrate/20220613080917_rename_description_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameDescriptionColumnToUsers < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :users, :description, :name
-end
-end
--- db/migrate/20220613105843_addpostcode_tousers.rb
+++
@@ -1,5 +0,0 @@
-class AddpostcodeTousers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :postcode, :integer, limit: 7
-  end
-end
