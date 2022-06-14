fc006e230d93ec8ab4e93adb2ca5f43e02fa9330
pachikuriii <stakahashi0728@gmail.com>
2022-06-15 06:47:53 +0900

    ユーザー一覧、個別ページ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     # サインアップ時にnameのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name postcode address introduction])
     # アカウント編集の時にnameとprofileのストロングパラメータを追加
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name postcode address introduction])
   end
 
   private
