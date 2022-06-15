29da8fec613feacb24920b6dbe4acac0067ba60f
pachikuriii <stakahashi0728@gmail.com>
2022-06-16 08:39:31 +0900

    i18nの設定方法を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   # deviseコントローラーにストロングパラメータを追加する
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action do
-    I18n.locale = :ja
-  end
+
 
   protected
 
