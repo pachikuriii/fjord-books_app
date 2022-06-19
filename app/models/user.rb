becbfdd303d2722a9816fdd32f3610089e422879
pachikuriii <stakahashi0728@gmail.com>
2022-06-19 13:11:29 +0900

    アソシエーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :followers,
+           class_name: 'Friendship',
+           foreign_key: 'follower_id',
+           dependent: :destroy,
+           inverse_of: :follower
+  has_many :followings,
+           class_name: 'Friendship',
+           foreign_key: 'following_id',
+           dependent: :destroy,
+           inverse_of: :following
+
+  has_many :following_users, through: :followers, source: :following
+  has_many :follower_users, through: :followings, source: :follower
 end
