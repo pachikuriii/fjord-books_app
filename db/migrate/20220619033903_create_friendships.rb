cc36896d95d067a5f2fdd566a4b5deb537e1c252
pachikuriii <stakahashi0728@gmail.com>
2022-06-19 12:54:01 +0900

    テーブルにインデックスを追加

--- db/migrate/20220619033903_create_friendships.rb
+++ db/migrate/20220619033903_create_friendships.rb
@@ -1,10 +1,13 @@
 class CreateFriendships < ActiveRecord::Migration[6.1]
   def change
     create_table :friendships do |t|
-      t.integer :follower_id
-      t.integer :following_id
+      t.integer :follower_id, null:false
+      t.integer :following_id, null:false
 
       t.timestamps
     end
+    add_index :friendships, :follower_id
+    add_index :friendships, :following_id
+    add_index :friendships, %i[follower_id following_id], unique: true
   end
 end
