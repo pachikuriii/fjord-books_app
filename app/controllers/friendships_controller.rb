d1bf5c3c3c35f660ac04fc46c70a106abd3e23ce
pachikuriii <stakahashi0728@gmail.com>
2022-06-24 09:25:39 +0900

    コントローラを設定

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # frozen_string_literal: true
 
 class FriendshipsController < ApplicationController
@@ -9,19 +8,6 @@ class FriendshipsController < ApplicationController
 
   def destroy
     current_user.unfollow(params[:id])
-=======
-class FriendshipsController < ApplicationController
-  # フォローするとき
-  def create
-    user = User.find(params[:following_id])
-    current_user.follow(params[:following_id])
-    redirect_to user_path(user)
-  end
-
-  # フォロー外すとき
-  def destroy
-    Friendship.find_by(follower_id: current_user.id, following_id: params[:id]).destroy
->>>>>>> b720128b931a4830fb30f9570a6cc5b560fbf521
     redirect_to user_path
   end
 end
