b24b6c2688f01e16e058ecf0f787ba297b482a46
pachikuriii <stakahashi0728@gmail.com>
2022-06-24 09:26:58 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books
-<<<<<<< HEAD
   resources :users, except: :create do
     member do
       get :followings, :followers
       resources :friendships, only: %i[create destroy] 
     end
   end
-=======
-  resources :users, only: %i(index show)
-  resources :users do
-    member do
-      get :followings, :followers
-    end
-  end
-  resources :friendships, only: [:create, :destroy]
->>>>>>> b720128b931a4830fb30f9570a6cc5b560fbf521
 end
