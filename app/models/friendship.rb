834c33a28a0342411493c30eb37ca836ac926709
pachikuriii <stakahashi0728@gmail.com>
2022-06-24 09:26:04 +0900

    アソシエーションの設定

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # frozen_string_literal: true
 
 class Friendship < ApplicationRecord
@@ -6,11 +5,4 @@ class Friendship < ApplicationRecord
   belongs_to :followed, class_name: 'User'
   validates :follower_id, presence: true
   validates :followed_id, presence: true
-=======
-class Friendship < ApplicationRecord
-  belongs_to :follower, class_name: 'User'
-  belongs_to :following, class_name: 'User'
-  validates :follower_id, presence: true
-  validates :following_id, presence: true
->>>>>>> b720128b931a4830fb30f9570a6cc5b560fbf521
 end
