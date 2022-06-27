e2521c44c83d6543385bd79fe4ae8d371b758dca
pachikuriii <stakahashi0728@gmail.com>
2022-06-27 15:12:49 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.with_attached_avatar.order(:id).page(params[:page])
+    @users = User.order(:id).page(params[:page]).with_attached_avatar
   end
 
   def show
