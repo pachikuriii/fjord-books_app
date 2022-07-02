a6416c75e5fbfe9e30df59909d0099f1a53084b3
pachikuriii <stakahashi0728@gmail.com>
2022-07-02 15:31:41 +0900

    マージに伴い設定変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_060958) do
+ActiveRecord::Schema.define(version: 2022_07_02_054757) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(version: 2022_07_02_060958) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.integer "postcode", limit: 7
     t.string "address"
     t.text "introduction"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
