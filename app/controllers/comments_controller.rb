bac98d0be119a05acf4fa67659f7a8dcdc24d048
pachikuriii <stakahashi0728@gmail.com>
2022-07-02 13:10:23 +0900

    リダイレクト部分を変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,36 +4,11 @@ class CommentsController < ApplicationController
   def create
     @comment = @commentable.comments.build(comment_params)
     @comment.user_id = current_user.id
-
     if @comment.save
-      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+      redirect_to polymorphic_url([@commentable]), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
     else
       render :new
     end
-  end
-
-  def edit
-    @comment = @commentable.comments.build(comment_params)
-    if @comment.save
-      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
-    else
-      render :new
-    end
-  end
-
-  def update
-    @comment = Comment.find(params[:id])
-    if @comment.update(comment_params)
-      redirect_to request.referer, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-    redirect_to request.referer, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 
   private
