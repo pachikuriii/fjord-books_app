eec7f2e9485789711a18ae00c51c3cca423d28a2
pachikuriii <stakahashi0728@gmail.com>
2022-07-08 22:02:58 +0900

    rubocopを通した

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,33 +1,35 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   setup do
     @alice = users(:alice)
     @bob = users(:bob)
     @carol = users(:carol)
-  end 
+  end
 
-  test "#following? フォローしている時" do
+  test '#following? フォローしている時' do
     assert_not @alice.following?(@bob)
 
     @alice.follow(@bob)
     assert @alice.following?(@bob)
   end
 
-  test "#followed_by? フォローされている時" do
+  test '#followed_by? フォローされている時' do
     assert_not @alice.following?(@bob)
     assert_not @bob.following?(@alice)
     @alice.follow(@bob)
     assert @bob.followed_by?(@alice)
   end
 
-  test "#follow フォローする時" do
+  test '#follow フォローする時' do
     assert_not @alice.following?(@bob)
     @alice.follow(@bob)
     assert @alice.following?(@bob)
   end
 
-  test "#unfollow フォロー解除する時" do
+  test '#unfollow フォロー解除する時' do
     assert_not @alice.following?(@bob)
     @alice.follow(@bob)
     assert @alice.following?(@bob)
@@ -35,10 +37,10 @@ class UserTest < ActiveSupport::TestCase
     assert_not @alice.following?(@bob)
   end
 
-  test "#name_or_email ユーザ情報に名前がない場合にメールアドレスを表示する時" do
+  test '#name_or_email ユーザ情報に名前がない場合にメールアドレスを表示する時' do
     assert_empty @carol.name
     assert_equal @carol.email, @carol.name_or_email
- 
+
     assert_not_empty @bob.name
     assert_not_equal @bob.email, @bob.name_or_email
   end
