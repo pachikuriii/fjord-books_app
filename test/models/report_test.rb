9d6811f9b288568475c53da69a8f477f837b2792
pachikuriii <stakahashi0728@gmail.com>
2022-07-08 23:22:05 +0900

    Reportクラスのインスタンスメソッドに対してモデルテストを書いた

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable? 投稿者が編集する時' do
+    current_user = users(:alice)
+    assert reports(:one).editable?(current_user)
+  end
+
+  test '#editable? 投稿者以外が編集する時' do
+    current_user = users(:carol)
+    assert_not reports(:one).editable?(current_user)
+  end
+
+  test '#created_on 作成日時をdateにする時' do
+    assert reports(:one).created_on.instance_of?(Date)
+  end
 end
