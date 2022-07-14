405dfbb21033f1228903918df130c02f1594bd85
pachikuriii <stakahashi0728@gmail.com>
2022-07-14 14:17:17 +0900

    respond toに関連する部分を削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,7 +9,7 @@ class ReportsController < ApplicationController
 
   # GET /reports/1 or /reports/1.json
   def show
-    @report = Report.includes(comments: :user).find(params[:id])
+    @report = Report.find(params[:id])
     @comments = @report.comments
     @comment = current_user.comments.build
   end
@@ -27,39 +27,29 @@ class ReportsController < ApplicationController
   # POST /reports or /reports.json
   def create
     @report = current_user.reports.build(report_params)
-    respond_to do |format|
+
       if @report.save
-        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
-        format.json { render :show, status: :created, location: @report }
+        redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) 
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity 
       end
-    end
   end
 
   # PATCH/PUT /reports/1 or /reports/1.json
   def update
     @report = Report.find(params[:id])
-    respond_to do |format|
       if @report.update(report_params)
-        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) }
-        format.json { render :show, status: :ok, location: @report }
+         redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) 
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
+        render :edit, status: :unprocessable_entity
       end
-    end
   end
 
   # DELETE /reports/1 or /reports/1.json
   def destroy
     @report = Report.find(params[:id])
     @report.destroy
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
-      format.json { head :no_content }
-    end
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
